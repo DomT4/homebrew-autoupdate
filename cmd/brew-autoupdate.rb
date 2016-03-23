632a7e51ced1abc1cfed4a3f6a0c6729f7f6a8ad
Kwaku Yeboah-Antwi <kwakuyeboah@gmail.com>
2016-03-23 23:22:45 +0100

    Added "--cleanup" flag to toggle automatically cleaning up after upgrade

--- cmd/brew-autoupdate.rb
+++ cmd/brew-autoupdate.rb
@@ -14,18 +14,22 @@ module Homebrew
         --stop = Stop autoupdating, but retain plist & logs.
         --delete = Cancel the autoupdate, delete the plist and logs.
         --upgrade = Also automatically upgrade your packages.
+        --cleanup = Automatically cleanup old packages after upgrade.
         --version = Output this tool's current version.
       EOS
     end
 
     if ARGV.include? "--version"
-      puts "Version 1.1.0, July 2015"
+      puts "Version 1.1.1, February 2016"
     end
 
     if ARGV.include? "--start"
       auto_args = "#{HOMEBREW_PREFIX}/bin/brew update"
       # Spacing at start of line is deliberate. Don't undo.
-      auto_args << " && #{HOMEBREW_PREFIX}/bin/brew upgrade -v" if ARGV.include? "--upgrade"
+      if ARGV.include? "--upgrade"
+        auto_args << " && #{HOMEBREW_PREFIX}/bin/brew upgrade -v"
+        auto_args << " && #{HOMEBREW_PREFIX}/bin/brew cleanup -v" if ARGV.include? "--cleanup"
+      end
 
       file = <<-EOS.undent
         <?xml version="1.0" encoding="UTF-8"?>
