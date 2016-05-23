0858554a417d9cc03414b28e06cd3aac20e210f1
Dominyk Tiller <dominyktiller@gmail.com>
2016-05-24 00:15:23 +0100

    autoupdate: rewrite command
    It was originally written as a Homebrew internal command, but we decided not
    to ship it at that point in time so I moved it over to supporting it myself
    for now, as there was reasonable demand for it and I use it personally.
    
    Now that it seems to be gaining usage it seemed a good idea to invest the time
    to rewrite the command properly, including stamping out some implementation hacks
    and making some style tweaks to keep Rubocop happy.
    
    Has been pretty thoroughly tested locally but please do shout if you run into
    any issues locally.

--- cmd/brew-autoupdate.rb
+++ cmd/brew-autoupdate.rb
@@ -1,79 +1,76 @@
 #!/usr/bin/env ruby -w
 
 require "fileutils"
-require "open-uri"
+require "utils"
 
-module Homebrew
-  def autoupdate
-    path = File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.autoupdate.plist")
+module Autoupdate
+  plist = File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.autoupdate.plist")
+  brew = HOMEBREW_PREFIX/"bin/brew"
 
-    if ARGV.empty? || ARGV.include?("--help") || ARGV.include?("-h")
-      puts <<-EOS.undent
-        Usage:
-        --start = Start autoupdating every 24 hours.
-        --stop = Stop autoupdating, but retain plist & logs.
-        --delete = Cancel the autoupdate, delete the plist and logs.
-        --upgrade = Also automatically upgrade your packages.
-        --cleanup = Automatically cleanup old packages after upgrade.
-        --version = Output this tool's current version.
-      EOS
+  if ARGV.empty? || ARGV.include?("--help") || ARGV.include?("-h")
+    puts <<-EOS.undent
+      Usage:
+      --start = Start autoupdating every 24 hours.
+      --stop = Stop autoupdating, but retain plist & logs.
+      --delete = Cancel the autoupdate, delete the plist and logs.
+      --upgrade = Also automatically upgrade your packages.
+      --cleanup = Automatically cleanup old packages after upgrade.
+      --version = Output this tool's current version.
+    EOS
+  end
+
+  if ARGV.include? "--version"
+    puts "Version 1.1.1, February 2016"
+  end
+
+  if ARGV.include? "--start"
+    auto_args = "#{brew} update"
+    # Spacing at start of lines is deliberate. Don't undo.
+    if ARGV.include? "--upgrade"
+      auto_args << " && #{brew} upgrade -v"
+      auto_args << " && #{brew} cleanup" if ARGV.include? "--cleanup"
     end
 
-    if ARGV.include? "--version"
-      puts "Version 1.1.1, February 2016"
-    end
+    file = <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>homebrew.mxcl.autoupdate</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>/bin/sh</string>
+            <string>-c</string>
+            <string>/bin/date && #{auto_args}</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.err</string>
+        <key>StandardOutPath</key>
+        <string>#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.out</string>
+        <key>StartInterval</key>
+        <integer>86400</integer>
+      </dict>
+      </plist>
+    EOS
 
-    if ARGV.include? "--start"
-      auto_args = "#{HOMEBREW_PREFIX}/bin/brew update"
-      # Spacing at start of line is deliberate. Don't undo.
-      if ARGV.include? "--upgrade"
-        auto_args << " && #{HOMEBREW_PREFIX}/bin/brew upgrade -v"
-        auto_args << " && #{HOMEBREW_PREFIX}/bin/brew cleanup -v" if ARGV.include? "--cleanup"
-      end
+    File.open(plist, "w") { |f| f << file }
+    quiet_system "/bin/launchctl", "load", plist
+    puts "Homebrew will now automatically update every 24 hours, or on system boot."
+  end
 
-      file = <<-EOS.undent
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-        <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>homebrew.mxcl.autoupdate</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>/bin/sh</string>
-              <string>-c</string>
-              <string>/bin/date && #{auto_args}</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>StandardErrorPath</key>
-          <string>#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.err</string>
-          <key>StandardOutPath</key>
-          <string>#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.out</string>
-          <key>StartInterval</key>
-          <integer>86400</integer>
-        </dict>
-        </plist>
-      EOS
+  if ARGV.include? "--stop"
+    quiet_system "/bin/launchctl", "unload", plist
+    puts "Homebrew will no longer autoupdate."
+  end
 
-      File.open(path, "w") { |f| f << file }
-      quiet_system "/bin/launchctl", "load", path
-      puts "Homebrew will now automatically update every 24 hours, or on system boot."
-    end
-
-    if ARGV.include? "--stop"
-      quiet_system "/bin/launchctl", "unload", path
-      puts "Homebrew will no longer autoupdate."
-    end
-
-    if ARGV.include? "--delete"
-      quiet_system "/bin/launchctl", "unload", path
-      rm_f path
-      rm_f "#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.err"
-      rm_f "#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.out"
-      puts "Homebrew will no longer autoupdate and the plist has been deleted."
-    end
+  if ARGV.include? "--delete"
+    quiet_system "/bin/launchctl", "unload", plist
+    FileUtils.rm_f plist
+    FileUtils.rm_f "#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.err"
+    FileUtils.rm_f "#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.out"
+    puts "Homebrew will no longer autoupdate and the plist has been deleted."
   end
 end
-
-Homebrew::autoupdate
