ac98082da74571c8e9ba5c12d78d7fbed905c336
Dominyk Tiller <dominyktiller@gmail.com>
2017-01-31 16:18:50 +0000

    autoupdate: change executable location
    It was pointed out third-party tools smash the user cache sometimes,
    which could break functionality here. Easy enough to make the executable
    persistent with a second plist but in the same of simplicity moving
    it hopefully one final time should resolve all remaining issues.
    
    I say that fully expecting someone to now find a new edge-case.

--- cmd/brew-autoupdate.rb
+++ cmd/brew-autoupdate.rb
@@ -2,10 +2,28 @@ require "fileutils"
 require "pathname"
 
 module Autoupdate
-  brew = HOMEBREW_PREFIX/"bin/brew"
-  plist = File.expand_path("~/Library/LaunchAgents/com.github.domt4.homebrew-autoupdate.plist")
-  logs = File.expand_path("~/Library/Logs/com.github.domt4.homebrew-autoupdate")
-  location = Pathname.new(File.expand_path("~/Library/Caches/com.github.domt4.homebrew-autoupdate"))
+  module_function
+
+  @brew = HOMEBREW_PREFIX/"bin/brew"
+  @name = "com.github.domt4.homebrew-autoupdate"
+  @plist = File.expand_path("~/Library/LaunchAgents/#{@name}.plist")
+  @logs = File.expand_path("~/Library/Logs/#{@name}")
+  @location = Pathname.new(File.expand_path("~/Library/Application Support/#{@name}"))
+
+  def cleanup
+    # Remove the old locations after a few months once it's safe to assume most
+    # existing users will have executed this command since the changes.
+    old_logs = Dir["#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.*"]
+    old_plist = File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.autoupdate.plist")
+    old_location = File.expand_path("~/Library/Caches/#{@name}")
+    FileUtils.rm_f old_logs
+    FileUtils.rm_f old_plist
+    FileUtils.rm_rf old_location
+
+    FileUtils.rm_f @plist
+    FileUtils.rm_rf @location
+    FileUtils.rm_rf @logs
+  end
 
   if ARGV.empty? || ARGV.include?("--help") || ARGV.include?("-h")
     puts <<-EOS.undent
@@ -22,7 +40,7 @@ module Autoupdate
   puts "Version 2.0.0. Last Changed: Jan 2017" if ARGV.include? "--version"
 
   if ARGV.include? "--start"
-    if File.exist?(plist)
+    if File.exist?(@plist)
       puts <<-EOS.undent
         The command already appears to have been started.
         Please run `brew autoupdate --delete` and try again.
@@ -33,18 +51,18 @@ module Autoupdate
     auto_args = "update"
     # Spacing at start of lines is deliberate. Don't undo.
     if ARGV.include? "--upgrade"
-      auto_args << " && #{brew} upgrade -v"
-      auto_args << " && #{brew} cleanup" if ARGV.include? "--cleanup"
+      auto_args << " && #{@brew} upgrade -v"
+      auto_args << " && #{@brew} cleanup" if ARGV.include? "--cleanup"
     end
 
     script_contents = <<-EOS.undent
       #!/bin/bash
-      /bin/date && #{brew} #{auto_args}
+      /bin/date && #{@brew} #{auto_args}
     EOS
-    FileUtils.mkpath(logs)
-    FileUtils.mkpath(location)
-    File.open(location/"updater", "w") { |sc| sc << script_contents }
-    FileUtils.chmod 0555, location/"updater"
+    FileUtils.mkpath(@logs)
+    FileUtils.mkpath(@location)
+    File.open(@location/"updater", "w") { |sc| sc << script_contents }
+    FileUtils.chmod 0555, @location/"updater"
 
     file = <<-EOS.undent
       <?xml version="1.0" encoding="UTF-8"?>
@@ -52,45 +70,39 @@ module Autoupdate
       <plist version="1.0">
       <dict>
         <key>Label</key>
-        <string>com.github.domt4.homebrew-autoupdate</string>
+        <string>#{@name}</string>
         <key>Program</key>
-        <string>#{location}/updater</string>
+        <string>#{@location}/updater</string>
         <key>ProgramArguments</key>
         <array>
-            <string>#{location}/updater</string>
+            <string>#{@location}/updater</string>
         </array>
         <key>RunAtLoad</key>
         <true/>
         <key>StandardErrorPath</key>
-        <string>#{logs}/com.github.domt4.homebrew-autoupdate.err</string>
+        <string>#{@logs}/#{@name}.err</string>
         <key>StandardOutPath</key>
-        <string>#{logs}/com.github.domt4.homebrew-autoupdate.out</string>
+        <string>#{@logs}/#{@name}.out</string>
         <key>StartInterval</key>
         <integer>86400</integer>
       </dict>
       </plist>
     EOS
 
-    File.open(plist, "w") { |f| f << file }
-    quiet_system "/bin/launchctl", "load", plist
+    File.open(@plist, "w") { |f| f << file }
+    quiet_system "/bin/launchctl", "load", @plist
     puts "Homebrew will now automatically update every 24 hours, or on system boot."
   end
 
   if ARGV.include? "--stop"
-    quiet_system "/bin/launchctl", "unload", plist
+    quiet_system "/bin/launchctl", "unload", @plist
     puts "Homebrew will no longer autoupdate."
   end
 
   if ARGV.include? "--delete"
-    quiet_system "/bin/launchctl", "unload", plist
-    FileUtils.rm_f plist
-    FileUtils.rm_rf location
-    FileUtils.rm_rf logs
-    # Old logs location. Keep around for a bit so we're not littering.
-    FileUtils.rm_f "#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.err"
-    FileUtils.rm_f "#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.out"
-    # Old plist name, adjusted for clarity of origin in version 2.0.0.
-    FileUtils.rm_f File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.autoupdate.plist")
+    quiet_system "/bin/launchctl", "unload", @plist
+    Autoupdate.cleanup
+
     puts "Homebrew will no longer autoupdate and the plist has been deleted."
   end
 end
