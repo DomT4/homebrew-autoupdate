a3ce6b24f5693cf59e0d7f23d8a97642d489fdf2
Dominyk Tiller <dominyktiller@gmail.com>
2017-02-12 20:15:17 +0000

    autoupdate: in-line usage instructions
    Takes advantage of Homebrew's semi-new native help mechanism
    for commands.

--- cmd/brew-autoupdate.rb
+++ cmd/brew-autoupdate.rb
@@ -1,10 +1,25 @@
+#:  * `autoupdate --start` [`--upgrade`] [`--cleanup`]:
+#:    Start autoupdating every 24 hours.
+#:
+#:    If `--upgrade` is specified, also automatically upgrade your packages.
+#:
+#:    If `--cleanup` is specified, also automatically cleanup old packages after upgrade.
+#:
+#:  * `autoupdate --stop`:
+#:    Stop autoupdating, but retain plist & logs.
+#:
+#:  * `autoupdate --delete`:
+#:    Cancel the autoupdate, delete the plist and logs.
+#:
+#:  * `autoupdate --version`:
+#:    Output this tool's current version.
+
 REPO = File.expand_path("#{File.dirname(__FILE__)}/..")
 LIBS = Pathname.new(REPO)/"lib"
 $LOAD_PATH.unshift(LIBS) unless $LOAD_PATH.include?(LIBS)
 
 require "autoupdate"
 
-puts Autoupdate.usage if ARGV.empty? || ARGV.include?("--help") || ARGV.include?("-h")
 Autoupdate.version if ARGV.include? "--version"
 Autoupdate.start if ARGV.include? "--start"
 Autoupdate.stop if ARGV.include? "--stop"
