5e784a0117d998d05cf5065180739a8387eaf2a3
Dominyk Tiller <dominyktiller@gmail.com>
2017-10-28 16:36:50 +0100

    cleanup: remove old location cleanup

--- lib/autoupdate/cleanup.rb
+++ lib/autoupdate/cleanup.rb
@@ -2,18 +2,9 @@ module Autoupdate
   module_function
 
   def cleanup
-    # Remove the old locations after a few months once it's safe to assume most
-    # existing users will have executed this command since the changes.
-    old_logs = Dir["#{HOMEBREW_PREFIX}/var/log/homebrew.mxcl.autoupdate.*"]
     fallback_logs = Dir[File.join(Autoupdate::Core.fallback_logs, "#{Autoupdate::Core.name}.*")]
-    old_plist = File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.autoupdate.plist")
-    old_location = File.expand_path("~/Library/Caches/#{Autoupdate::Core.name}")
 
-    FileUtils.rm_f old_logs
     FileUtils.rm_f fallback_logs
-    FileUtils.rm_f old_plist
-    FileUtils.rm_rf old_location
-
     FileUtils.rm_f Autoupdate::Core.plist
     FileUtils.rm_rf Autoupdate::Core.location
     FileUtils.rm_rf Autoupdate::Core.logs
