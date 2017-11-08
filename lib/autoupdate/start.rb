bc68ec60ced5decc81e734397c74e1418ecceea4
Dominyk Tiller <dominyktiller@gmail.com>
2017-11-08 01:34:47 +0000

    start: use /bin/sh by default
    Trying to be clever with ENV["SHELL"] hasn't completely stomped out
    the issue using it was trying to resolve in 488d5691ea4d031506a79d254f6635c7a72f3376, so temporarily
    going back to a sensible default whilst I try and look at the issue.

--- lib/autoupdate/start.rb
+++ lib/autoupdate/start.rb
@@ -26,7 +26,7 @@ module Autoupdate
     end
 
     script_contents = <<~EOS
-      #!#{ENV["SHELL"]}
+      #!/bin/sh
       /bin/date && #{Autoupdate::Core.brew} #{auto_args}
     EOS
     FileUtils.mkpath(Autoupdate::Core.logs)
