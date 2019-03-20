1dcc78366eb8e79778370408ce7927da9d0c263f
Dominyk Tiller <dominyktiller@gmail.com>
2019-03-20 11:13:21 +0000

    status: add new `status` command
    Closes https://github.com/DomT4/homebrew-autoupdate/issues/22.

---
+++ lib/autoupdate/status.rb
@@ -0,0 +1,33 @@
+module Autoupdate
+  module_function
+
+  def autoupdate_running?
+    # Method from Homebrew.
+    # https://github.com/Homebrew/brew/blob/c9c7f4/Library/Homebrew/utils/popen.rb
+    Utils.popen_read("/bin/launchctl list").include?(Autoupdate::Core.name)
+  end
+
+  def autoupdate_installed_but_stopped?
+    File.exist?(Autoupdate::Core.location/"updater") && !autoupdate_running?
+  end
+
+  def autoupdate_not_installed?
+    !File.exist?(Autoupdate::Core.plist)
+  end
+
+  def status
+    if autoupdate_running?
+      puts "Autoupdate is installed and running."
+    elsif autoupdate_installed_but_stopped?
+      puts "Autoupdate is installed but stopped."
+    elsif autoupdate_not_installed?
+      puts "Autoupdate does not seem to be installed."
+    else
+      puts <<~EOS
+        Autoupdate cannot determine its status.
+        Please feel free to file an issue with further information here:
+          https://github.com/DomT4/homebrew-autoupdate/issues
+      EOS
+    end
+  end
+end
