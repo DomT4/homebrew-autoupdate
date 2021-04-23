85fc920011b82c2012674bae599f01dd8c7b85f6
Dominyk Tiller <dominyktiller@gmail.com>
2021-04-23 03:05:10 +0100

    stop: prevent repeated pointless stops

--- lib/autoupdate/stop.rb
+++ lib/autoupdate/stop.rb
@@ -3,7 +3,17 @@
 module Autoupdate
   module_function
 
+  def check_is_not_already_stopped
+    unless Utils.popen_read("/bin/launchctl", "list").include?(Autoupdate::Core.name)
+      odie <<~EOS
+        Autoupdate is not currently running; cannot stop a command that is not running.
+      EOS
+    end
+  end
+
   def stop
+    check_is_not_already_stopped
+
     quiet_system "/bin/launchctl", "unload", Autoupdate::Core.plist
     puts "Homebrew will no longer autoupdate."
   end
