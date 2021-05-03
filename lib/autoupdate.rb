26e424460a51ffba5b94964a622d49ad260cd28e
Dominyk Tiller <dominyktiller@gmail.com>
2021-05-03 23:45:01 +0100

    autoupdate: alphabetise; require tap/formula

--- lib/autoupdate.rb
+++ lib/autoupdate.rb
@@ -2,9 +2,11 @@
 
 require "date"
 require "fileutils"
+require "formula"
 require "pathname"
-require_relative "autoupdate/core"
+require "tap"
 require_relative "autoupdate/cleanup"
+require_relative "autoupdate/core"
 require_relative "autoupdate/delete"
 require_relative "autoupdate/notify"
 require_relative "autoupdate/start"
